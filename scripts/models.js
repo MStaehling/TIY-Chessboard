7d8b24a397b6b667b6481c6ad44e9dec43ef1c39
Michael Staehling <Michael.staehling@gmail.com>
2015-05-26 23:18:29 -0400

    chest moves

---
+++ scripts/models.js
@@ -0,0 +1,69 @@
+$function(){
+  var board = null;
+  var moves = [];
+  function initBoard(){
+    return  [
+  ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r'] ];
+  };
+
+  function move(board, fromX, fromY, toX, toY){
+    board[fromX][fromY] = board[toX][toY];
+    board[toX][toY] = ' ';
+    console.log(board.join('\n'));
+  };
+  
+  // move(initBoard, 6, 3, 4, 3);
+  // move(initBoard, 0, 6, 2, 5);
+  // move(initBoard, 6, 2, 4, 2);
+  // move(initBoard, 1, 4, 2, 4);
+  // move(initBoard, 6, 6, 5, 6);
+  // move(initBoard, 1, 3, 3, 3);
+  // move(initBoard, 7, 5, 6, 6);
+  // move(initBoard, 0, 5, 1, 4);
+  // move(initBoard, 7, 6, 5, 5);
+  // move(initBoard, 0, 4, 0, 6);
+  // move(initBoard, 0, 7, 0, 5);
+  // move(initBoard, 7, 4, 7, 6);
+  // move(initBoard, 7, 7, 7, 5);
+  // move(initBoard, 3, 3, 4, 2);
+  // move(initBoard, 7, 3, 6, 2);
+  // move(initBoard, 1, 0, 2, 0);
+  // move(initBoard, 6, 2, 4, 2);
+  // move(initBoard, 1, 1, 3, 1);
+  // move(initBoard, 4, 2, 6, 2);
+  // move(initBoard, 0, 2, 1, 1);
+  // move(initBoard, 7, 2, 6, 3);
+  // move(initBoard, 1, 1, 4, 4);
+  // move(initBoard, 6, 2, 7, 2);
+  // move(initBoard, 4, 4, 1, 1);
+  // move(initBoard, 6, 3, 5, 4);
+  // move(initBoard, 2, 5, 3, 3);
+  // move(initBoard, 7, 1, 5, 2);
+  // move(initBoard, 0, 1, 1, 3);
+  // move(initBoard, 7, 5, 7, 3);
+  // move(initBoard, 0, 0, 0, 2);
+  // move(initBoard, 5, 2, 3, 3);
+  // move(initBoard, 1, 1, 3, 3);
+  // move(initBoard, 5, 5, 7, 4);
+  // move(initBoard, 1, 2, 2, 2);
+  // move(initBoard, 7, 4, 5, 3);
+  // move(initBoard, 0, 3, 2, 1);
+  // move(initBoard, 7, 2, 5, 2);
+  // move(initBoard, 3, 1, 4, 1);
+  // move(initBoard, 5, 2, 6, 3);
+  // move(initBoard, 2, 0, 3, 0);
+  // move(initBoard, 7, 3, 7, 2);
+
+
+
+
+
+
+};

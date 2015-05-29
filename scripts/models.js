415e294fe5d257e57d67c0195155705c0adfa1f2
Michael Staehling <Michael.staehling@gmail.com>
2015-05-29 15:07:21 -0400

    adding test files

--- scripts/models.js
+++ scripts/models.js
@@ -1,24 +1,27 @@
-$function(){
-  var board = null;
-  var moves = [];
-  function initBoard(){
-    return  [
-  ['R','N','B','Q','K','B','N','R'],
-  ['P','P','P','P','P','P','P','P'],
-  [' ',' ',' ',' ',' ',' ',' ',' '],
-  [' ',' ',' ',' ',' ',' ',' ',' '],
-  [' ',' ',' ',' ',' ',' ',' ',' '],
-  [' ',' ',' ',' ',' ',' ',' ',' '],
-  ['p','p','p','p','p','p','p','p'],
-  ['r','n','b','q','k','b','n','r'] ];
-  };
+(function(window){
+  window.chess = {
 
-  function move(board, fromX, fromY, toX, toY){
-    board[fromX][fromY] = board[toX][toY];
-    board[toX][toY] = ' ';
-    console.log(board.join('\n'));
   };
-  
+  // var board = null;
+  // var moves = [];
+  // function initBoard(){
+  //   return  [
+  // ['R','N','B','Q','K','B','N','R'],
+  // ['P','P','P','P','P','P','P','P'],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // ['p','p','p','p','p','p','p','p'],
+  // ['r','n','b','q','k','b','n','r'] ];
+  // };
+  //
+  // function move(board, fromX, fromY, toX, toY){
+  //   board[fromX][fromY] = board[toX][toY];
+  //   board[toX][toY] = ' ';
+  //   console.log(board.join('\n'));
+  // };
+
   // move(initBoard, 6, 3, 4, 3);
   // move(initBoard, 0, 6, 2, 5);
   // move(initBoard, 6, 2, 4, 2);
@@ -66,4 +69,4 @@ $function(){
 
 
 
-};
+})(window);

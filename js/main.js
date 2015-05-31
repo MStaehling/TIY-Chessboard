4842a3d0de41a4258b0da2187234bc50361cc5c0
Michael Staehling <Michael.staehling@gmail.com>
2015-05-31 15:11:56 -0400

    removing some files unneeded files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(window){
+
+$('.first').click(function() {
+  // $('.rank-2 > .file-d').removeClass('white.pawn');
+  // $('.rank-4 . .file-d').addClass('white.pawn');
+  console.log("hi");
+})
+
+
+
+})(window);

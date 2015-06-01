3907e720ef3b01dfb3f8a9cd8f3414d035089b78
Michael Staehling <Michael.staehling@gmail.com>
2015-05-31 20:45:53 -0400

    movin pieces with @sjoyal @jessyriordan @pcreglow @epicurean304 @jorgehjr84

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-(function(window){
+(function(window) {
 
-$('.first').click(function() {
-  // $('.rank-2 > .file-d').removeClass('white.pawn');
-  // $('.rank-4 . .file-d').addClass('white.pawn');
-  console.log("hi");
-})
+
+//got working with sean
+$('.next').click(function() {
+  chess.counter = (chess.counter + 1)% chess.game.length;
+  chess.move(chess.game[chess.counter]);
+});
+
+$('.last').click(function(){
+  for (i = 0; i < chess.game.length; i++) {
+    chess.move(chess.game[i]);
+  };
+});
+
+
+
+
+
+
+// Old function
+// $('.last').click(function(){
+//   chess.move(chess.game.move1);
+//   chess.move(chess.game.move2);
+//   chess.move(chess.game.move3);
+//   chess.move(chess.game.move4);
+//   chess.move(chess.game.move5);
+//   chess.move(chess.game.move6);
+//   chess.move(chess.game.move7);
+//   chess.move(chess.game.move8);
+//   chess.move(chess.game.move9);
+// });
 
 
 
